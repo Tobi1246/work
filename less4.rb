3032c1b4d2aa240aba22ea928c24110e279ff47e
Tobi1246 <92755680+Tobi1246@users.noreply.github.com>
2022-08-10 13:42:46 +0300

    Add files via upload

---
+++ less4.rb
@@ -0,0 +1,21 @@
+
+puts "Введите значение a"
+a=gets.chomp.to_f
+
+puts "Введите значение b"
+b=gets.chomp.to_f
+
+puts "Введите  значение c"
+c=gets.chomp.to_f
+
+
+d=(b**2)-(4*a*c)
+if d<0 
+	puts "Нет корней"
+else 
+	x1= (-b+Math.sqrt(b))/2*a.to_f
+	x2= (-b-Math.sqrt(b))/2*a.to_f
+puts "x1=#{x1}"
+puts "x2=#{x2}"
+end
+
