fdea4a5b0128cfed8d0b447f438c50350ab70e22
Tobi1246 <92755680+Tobi1246@users.noreply.github.com>
2022-08-10 12:25:39 +0300

    Add files via upload

---
+++ less1.rb
@@ -0,0 +1,13 @@
+puts "Привет! , Введи своё Имя "
+name=gets.chomp.capitalize
+
+puts "Введите #{name} ваш рост  "
+height=Float(gets.chomp)
+
+ideal_wt=(height - 110) * 1.15
+if ideal_wt<0
+    puts " #{name} , ваш вес уже оптимальный "
+
+else 
+     puts " #{name}, Ваш вес не оптимальный "
+end
