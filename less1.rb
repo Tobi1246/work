d83eeed88bf7fb8d5c864c759161079de4f42b24
Tobi1246 <92755680+Tobi1246@users.noreply.github.com>
2022-08-10 19:50:07 +0300

    Update less1.rb

--- less1.rb
+++ less1.rb
@@ -4,10 +4,9 @@ name=gets.chomp.capitalize
 puts "Введите #{name} ваш рост  "
 height=Float(gets.chomp)
 
-ideal_wt=(height - 110) * 1.15
-if ideal_wt<0
+ideal_wt = (height - 110) * 1.15
+if ideal_wt < 0
     puts " #{name} , ваш вес уже оптимальный "
-
 else 
      puts " #{name}, Ваш вес не оптимальный "
 end
