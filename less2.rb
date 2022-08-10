3a77dd720e74bf0eba9e959d5c329cdcd590ba2e
Tobi1246 <92755680+Tobi1246@users.noreply.github.com>
2022-08-10 12:44:52 +0300

    Add files via upload

---
+++ less2.rb
@@ -0,0 +1,8 @@
+puts "Введите основание треугольника"
+base_t=Float(gets.chomp)
+
+puts "Введите высоту треугольника "
+height=Float(gets.chomp)
+ square=(base_t*height)*0.5
+ puts square
+ puts "Площать треугольника=#{square}"
