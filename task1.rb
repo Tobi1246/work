03784b3a1ca34eacf68b86f91241487373b7d1da
Tobi1246 <92755680+Tobi1246@users.noreply.github.com>
2022-08-12 10:05:10 +0300

    Add files via upload

---
+++ task1.rb
@@ -0,0 +1,15 @@
+ears = {'January' => 31,
+	'February' => 28,
+  'March' => 31,
+  'April' => 30,
+  'May' => 31,
+  'June' => 30,
+  'July' => 31,
+  'August' => 31,
+  'September' => 30,
+  'October' => 31,
+  'November' => 30,
+  'December' => 31}
+ears.each do |name, number|
+  puts name if number >= 30
+end
