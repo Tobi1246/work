164331a492836760297b20018c4bb65d21d2d0eb
Tobi1246 <92755680+Tobi1246@users.noreply.github.com>
2022-08-15 12:02:26 +0300

    Add files via upload

---
+++ task5.rb
@@ -0,0 +1,27 @@
+m_day = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+puts "Введите данные"
+puts "Дата"
+date = gets.chomp.to_i
+
+puts "Месяц"
+month = gets.chomp.to_i
+
+puts "Год"
+year = gets.chomp.to_i
+
+n_date = 0
+
+for i in 0..month - 2
+  n_date += m_day[i]
+end
+if (year % 4 == 0)
+
+elsif ((year % 400 == 0) && (year % 100 == 0))
+  n_date += 1
+end
+
+n_date = date + n_date
+
+puts "Порядковый номер даты #{n_date}"
+
+
