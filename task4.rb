3f5deaba799d0e3fd9434f0a7b75e0d199fda554
Tobi1246 <92755680+Tobi1246@users.noreply.github.com>
2022-08-15 10:56:38 +0300

    Add files via upload

---
+++ task4.rb
@@ -0,0 +1,11 @@
+ALPHABET = ('a'..'z').to_a
+vowels = ["a", "e", "i", "o", "u", "y"]
+hash_abc = {}
+
+ALPHABET.each_with_index do |letter, i|
+	
+		hash_abc[letter] = i + 1 if vowels.index.include?(letter) 	
+end
+puts hash_abc
+
+	
