efdeba39a22ed9828e07df757d04dfcafcd16cfb
Tobi1246 <92755680+Tobi1246@users.noreply.github.com>
2022-08-16 09:12:52 +0300

    Update task4.rb

--- task4.rb
+++ task4.rb
@@ -1,10 +1,8 @@
 ALPHABET = ('a'..'z').to_a
 vowels = ["a", "e", "i", "o", "u", "y"]
 hash_abc = {}
-
 ALPHABET.each_with_index do |letter, i|
-	
-		hash_abc[letter] = i + 1 if vowels.index.include?(letter) 	
+  hash_abc[letter] = i + 1 if vowels.index.include?(letter) 	
 end
 puts hash_abc
 
