69e8d7b895ce93e982cc0f193d4d99dd5e4841bd
Tobi1246 <92755680+Tobi1246@users.noreply.github.com>
2022-08-16 09:17:02 +0300

    Update task6.rb

--- task6.rb
+++ task6.rb
@@ -2,15 +2,15 @@ puts "Добро пожаловать в наш магазин, выберите
 puts "Для завершения покупок или выхода введите stop в строке названия товара"
 catalog = {}
 cost = 0 
-  loop  do
-    puts "Название тавара"
-    name_t = gets.chomp
-    break if name_t == "stop" 
-    puts "Стоимость тавара"
-    name_price = gets.chomp.to_f
-    puts "Ко-во тавара"
-    name_quanta = gets.chomp.to_i
-    catalog[name_t] = {name_price=>name_quanta}     
+loop  do
+  puts "Название тавара"
+  name_t = gets.chomp
+  break if name_t == "stop" 
+  puts "Стоимость тавара"
+  name_price = gets.chomp.to_f
+  puts "Ко-во тавара"
+  name_quanta = gets.chomp.to_i
+  catalog[name_t] = {name_price=>name_quanta}     
 end
   catalog.each do |name_t, name_price|
   catalog = name_price.keys.first * name_price.values.first
