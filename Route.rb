23808a34e51385744256b4b5768a065959a038aa
Tobi1246 <92755680+Tobi1246@users.noreply.github.com>
2022-08-26 11:28:24 +0300

    Add files via upload

---
+++ Route.rb
@@ -0,0 +1,23 @@
+class Route < Station
+  attr_reader :station
+  def initialize(first_station, last_station)
+    @first_station = first_station
+    @last_station = last_station
+    @station = []
+    @station << first_station
+    @station << last_station
+  end
+
+    def station(station)
+      @station.insert(-2,station)
+    end
+
+    def delete_station(station)
+      @station.delete(station)
+    end
+
+  def  all_station
+    @station.each do |keys|
+    end
+  end
+end
