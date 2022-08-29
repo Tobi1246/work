3b8d97316ac3762fbe5e913d1639a4ed1afd5aac
Tobi1246 <92755680+Tobi1246@users.noreply.github.com>
2022-08-29 18:48:16 +0300

    Update and rename Route.rb to route.rb

---
+++ route.rb
@@ -0,0 +1,21 @@
+class Route 
+  attr_reader :start_station, :end_station, :station
+
+  def initiolize (first_station, last_station )
+    @start_station = first_station
+    @end_station = last_station
+    @station = [first_station, last_station]
+  end
+
+  def add_station(station)
+    @station.insert(-2,station)
+  end
+
+  def delete_station(station)
+    @station.delete(station)
+  end
+
+  def  all_station
+   @station
+  end
+end
