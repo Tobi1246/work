de986736f8cb29b770c403102dc2c694f512bf67
Tobi1246 <92755680+Tobi1246@users.noreply.github.com>
2022-10-03 10:09:51 +0300

    Update cargo_wagon.rb

--- less5/cargo_wagon.rb
+++ less5/cargo_wagon.rb
@@ -12,11 +12,10 @@ class CargoWagon < Wagon
     if @steal_v == @size_v
       puts "#{@steal_v} = #{@size_v} больше места нету "
     else
-    volume = volume.to_i
-    @steal_v += volume  if (@size_v - @steal_v) >= volume
-    @free = @size_v - @steal_v
-    puts "Вы заняли : #{@steal_v} от общего объёма : #{@size_v},свободного объёма : #{@free}"
+      volume = volume.to_i
+      @steal_v += volume  if (@size_v - @steal_v) >= volume
+      @free = @size_v - @steal_v
+      puts "Вы заняли : #{@steal_v} от общего объёма : #{@size_v},свободного объёма : #{@free}"
     end
   end
-
 end
